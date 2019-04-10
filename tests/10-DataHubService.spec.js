694939153c29b62ae374bb17a15c9f5635682237
David I. Lehn <dlehn@digitalbazaar.com>
2019-04-10 13:35:31 -0400

    Add tests.
    - Work-in-progres: Tests do not run in node or karma.
    - Pulling in tests from bedrock-web-data-hub.
    - Add karma support.
    - Add package file list.
    - Add esm dependency.

---
+++ tests/10-DataHubService.spec.js
@@ -0,0 +1,86 @@
+/*!
+ * Copyright (c) 2018-2019 Digital Bazaar, Inc. All rights reserved.
+ */
+//import {DataHubService} from 'data-hub-client';
+import {DataHubService} from '..';
+import {mock} from './mock.js';
+
+describe('DataHubService', () => {
+  before(async () => {
+    await mock.init();
+  });
+  after(async () => {
+    mock.server.shutdown();
+  });
+
+  it('should create data hub storage', async () => {
+    const dhs = new DataHubService();
+    const {kek, hmac} = mock.keys;
+    const config = await dhs.create({
+      config: {
+        sequence: 0,
+        controller: mock.accountId,
+        kek: {id: kek.id, algorithm: kek.algorithm},
+        hmac: {id: hmac.id, algorithm: hmac.algorithm}
+      }
+    });
+    config.should.be.an('object');
+    config.id.should.be.a('string');
+    config.controller.should.equal(mock.accountId);
+    config.kek.should.be.an('object');
+    config.hmac.should.be.an('object');
+  });
+
+  it('should get data hub storage', async () => {
+    const dhs = new DataHubService();
+    const {kek, hmac} = mock.keys;
+    const {id} = await dhs.create({
+      config: {
+        sequence: 0,
+        controller: mock.accountId,
+        kek: {id: kek.id, algorithm: kek.algorithm},
+        hmac: {id: hmac.id, algorithm: hmac.algorithm}
+      }
+    });
+    const config = await dhs.get({id});
+    config.should.be.an('object');
+    config.id.should.be.a('string');
+    config.controller.should.equal(mock.accountId);
+    config.kek.should.be.an('object');
+    config.hmac.should.be.an('object');
+  });
+
+  it('should create primary data hub storage', async () => {
+    const dhs = new DataHubService();
+    const {kek, hmac} = mock.keys;
+    const config = await dhs.create({
+      config: {
+        sequence: 0,
+        controller: mock.accountId,
+        kek: {id: kek.id, algorithm: kek.algorithm},
+        hmac: {id: hmac.id, algorithm: hmac.algorithm},
+        primary: true
+      }
+    });
+    config.should.be.an('object');
+    config.id.should.be.a('string');
+    config.controller.should.equal(mock.accountId);
+    config.kek.should.be.an('object');
+    config.hmac.should.be.an('object');
+  });
+
+  it('should get primary data hub storage', async () => {
+    // Note: depends on previous test that created primary data hub
+    // could alternatively change `before/after` to `beforeEach/afterEach`
+    // to enable creating multiple primary data hubs
+    const dhs = new DataHubService();
+    const config = await dhs.getPrimary({controller: mock.accountId});
+    config.should.be.an('object');
+    config.id.should.be.a('string');
+    config.controller.should.equal(mock.accountId);
+    config.kek.should.be.an('object');
+    config.hmac.should.be.an('object');
+  });
+
+  // TODO: add more tests: getAll, update, setStatus
+});
