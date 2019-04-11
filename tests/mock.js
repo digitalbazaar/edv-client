992675668dc883edaef450811532ed18fca33eb2
andrewjones <ajones@digitalbazaar.com>
2019-04-10 19:55:57 -0500

    Add working tests in both Karma and Node.
    Remove crypto error & console.logs.
    
    Generate a mock key.
    
    Remove bedrock-web-kms from test project.
    
    Switching back to get HMAC stuff.
    
    Add id to mock HMAC.
    
    Add should to eslintrc.js globals.
    
    Closer to removing real kmsservice from project entirely.
    
    Remove ExtendMockKeyService.
    
    Fix await in wrapKey.
    
    Remove need for bedrock-web-kms for karma.
    
    Use kmsPlugin in mockMasterKey.
    
    Remove code to stub out Pretender.
    
    Mock tests (#2)
    
    * Remove mocha setup.
    
    * First test working in node.
    
    * Ok a node test really succeeds this time.
    
    * Move closer to using sinon for all mocks.
    
    * All tests run in karma using sinon stubs.
    
    * All tests passing in both node and karma.
    
    * Added doc strings.

--- tests/mock.js
+++ tests/mock.js
@@ -1,82 +1,86 @@
 /*!
  * Copyright (c) 2018-2019 Digital Bazaar, Inc. All rights reserved.
  */
-import Pretender from 'pretender';
 import {DataHubClient, DataHubService} from '..';
 import {MockStorage} from 'bedrock-web-mock-data-hub-storage';
 import {MockKmsService} from 'bedrock-web-mock-kms-http';
+import {getMockKey} from './generateTestKey';
+
 // FIXME use mock test data
-//import {AccountMasterKey, KmsService} from 'bedrock-web-kms';
 
-export const mock = {};
+let instance = null;
 
-// TODO: add some static data to test against
-
-mock.init = async () => {
-  const accountId = mock.accountId = 'test';
-
-  // create mock server
-  const server = mock.server = new Pretender();
-  // FIXME: there is special behavior here that the Mock* classes rely on;
-  // we need to clean that up
-  server.prepareHeaders = function(headers) {
-    if(headers) {
-      if(headers.json) {
-        headers['content-type'] = 'application/json';
-        delete headers.json;
+class TestMock {
+  constructor(env = process.env.NODE_ENV) {
+    if(instance !== null) {
+      return instance;
+    }
+    instance = this;
+    // create mock server
+    const MockServer = require('./mockNodeServer');
+    this.server = new MockServer();
+    // mock backend for KMS
+    this.kms = new MockKmsService({server: this.server});
+    const accountId = this.accountId = 'test';
+    // mock data hub storage
+    this.dataHubStorage = new MockStorage(
+      {server: this.server, controller: accountId});
+  }
+  preparePretender() {
+    if(this.server) {
+      return null;
+    }
+    const Pretender = require('pretender');
+    this.server = new Pretender();
+    this.server.prepareHeaders = function(headers) {
+      if(headers) {
+        if(headers.json) {
+          headers['content-type'] = 'application/json';
+          delete headers.json;
+        }
+      } else {
+        headers = {};
       }
-    } else {
-      headers = {};
-    }
-    return headers;
-  };
-  server.prepareBody = function(body, headers) {
-    if(headers && headers['content-type'] === 'application/json') {
-      return (body && typeof body !== 'string') ?
-        JSON.stringify(body) : '{"message": "mock server error"}';
-    }
-    return body;
-  };
-
-  // mock backend for KMS
-  mock.kms = new MockKmsService({server});
-
-  // mock data hub storage
-  mock.dataHubStorage = new MockStorage({server, controller: accountId});
-
-  // only init keys once
-  if(!mock.keys) {
-    // create mock keys
-    mock.keys = {};
-
-    // account master key for using KMS
-    const secret = 'bcrypt of password';
-    // FIXME use mock data
-    //const kmsService = new KmsService();
-    //mock.keys.master = await AccountMasterKey.fromSecret(
-    //  {secret, accountId, kmsService, kmsPlugin: 'mock'});
-
-    //// create KEK and HMAC keys for creating data hubs
-    //mock.keys.kek = await mock.keys.master.generateKey({type: 'kek'});
-    //mock.keys.hmac = await mock.keys.master.generateKey({type: 'hmac'});
-    mock.keys.kek = null; // FIXME
-    mock.keys.hmac = null; // FIXME
+      return headers;
+    };
+    this.server.prepareBody = function(body, headers) {
+      if(headers && headers['content-type'] === 'application/json') {
+        return (body && typeof body !== 'string') ?
+          JSON.stringify(body) : '{"message": "mock server error"}';
+      }
+      return body;
+    };
   }
-};
+  async init() {
+    // only init keys once
+    if(!this.keys) {
+      // create mock keys
+      this.keys = {};
 
-mock.createDataHub = async (
-  {controller = mock.accountId, primary = false} = {}) => {
-  const dhs = new DataHubService();
-  const {kek, hmac} = mock.keys;
-  let config = {
-    sequence: 0,
-    controller,
-    kek: {id: kek.id, algorithm: kek.algorithm},
-    hmac: {id: hmac.id, algorithm: hmac.algorithm}
-  };
-  if(primary) {
-    config.primary = true;
+      // account master key for using KMS
+      this.keys.master = await getMockKey({kmsService: this.kms});
+      // create KEK and HMAC keys for creating data hubs
+      this.keys.kek = await this.keys.master.generateKey({type: 'kek'});
+      this.keys.hmac = await this.keys.master.generateKey({type: 'hmac'});
+    }
   }
-  config = await dhs.create({config});
-  return new DataHubClient({config, kek, hmac});
-};
+  async createDataHub(
+    {controller = this.accountId, primary = false} = {}) {
+    const dhs = new DataHubService();
+    const {kek, hmac} = this.keys;
+    let config = {
+      sequence: 0,
+      controller,
+      kek: {id: kek.id, algorithm: kek.algorithm},
+      hmac: {id: hmac.id, algorithm: hmac.algorithm}
+    };
+    if(primary) {
+      config.primary = true;
+    }
+    config = await dhs.create({config});
+    return new DataHubClient({config, kek, hmac});
+  }
+}
+
+const singleton = new TestMock();
+export default singleton;
