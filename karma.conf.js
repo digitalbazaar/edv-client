b85361b0c1e5b660b490617dc94373148ad4809d
andrewjones <ajones@digitalbazaar.com>
2019-04-16 16:38:37 -0500

    Remove trailing commas & env check from TestMock.

--- karma.conf.js
+++ karma.conf.js
@@ -14,7 +14,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'tests/test-karma.js',
-      'tests/*.spec.js',
+      'tests/*.spec.js'
     ],
 
     // list of files to exclude
@@ -23,7 +23,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'tests/*.js': ['webpack', 'babel', 'sourcemap'],
+      'tests/*.js': ['webpack', 'babel', 'sourcemap']
     },
 
     webpack: {
