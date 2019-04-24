fcef6a95e02b11d1c57b4cc7626d5c63af546622
David I. Lehn <dlehn@digitalbazaar.com>
2019-04-23 21:42:40 -0400

    Move sub-dependency crypto fix back to tests.
    - Setting global temporarily to avoid fixing Node.js test issues with
      sub-dependencies that also use WebCrypto.

--- crypto.js
+++ crypto.js
@@ -1,6 +1,2 @@
 // WebCrypto polyfill
 export {default} from 'isomorphic-webcrypto';
-
-// FIXME: here to avoid fixing sub-dependencies
-import {default as crypto} from 'isomorphic-webcrypto';
-global.crypto = crypto;
