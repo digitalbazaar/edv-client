5355a679e8694ec93b03a39597c0159ccd822c36
Andrew Jones <ajones@digitalbazaar.com>
2019-08-16 18:28:30 -0400

    Add MockControllerKey.

--- tests/MockControllerKey.js
+++ tests/MockControllerKey.js
@@ -1,5 +1,20 @@
+import crypto from '../crypto.js';
+
 export class MockControllerKey {
   constructor(seed) {
     this.seed = seed;
+  }
+  static async create() {
+    const extractable = true;
+    const key = await crypto.subtle.importKey(
+      'raw', 'test', {name: 'HMAC', hash: {name: 'SHA-256'}}, extractable,
+      ['sign', 'verify']);
+    return {
+      id: `did:key:test`,
+      type: key.type,
+      sign({data}) {
+        return key.sign({data});
+      }
+    };
   }
 }
