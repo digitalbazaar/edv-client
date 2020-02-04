afc7694492c573ade32aae51c4a8fafd389dff54
Andrew Jones <ajones@digitalbazaar.com>
2020-02-04 15:47:02 -0500

    Start stream read tests.

--- tests/30-EdvStreams.spec.js
+++ tests/30-EdvStreams.spec.js
@@ -3,7 +3,7 @@
  */
 'use strict';
 
-import {EdvClient} from '..';
+import {EdvDocument, EdvClient} from '..';
 import mock from './mock.js';
 import {isRecipient, isNewEDV} from './test-utils.js';
 import {ReadableStream} from '../util';
@@ -49,6 +49,57 @@ describe('EDV Stream Tests', function() {
     inserted.content.should.deep.equal({someKey: 'someValue'});
     should.exist(inserted.stream);
     inserted.stream.should.be.an('object');
+  });
+
+  it('should be able to decrypt a stream from an EdvDocument', async () => {
+    const {invocationSigner, keyResolver} = mock;
+    const client = await mock.createEdv();
+    client.ensureIndex({attribute: 'content.indexedKey'});
+    const doc1Id = await EdvClient.generateId();
+    const doc1 = {id: doc1Id, content: {indexedKey: 'value1'}};
+    const data = getRandomUint8();
+    const stream = new ReadableStream({
+      pull(controller) {
+        controller.enqueue(data);
+        controller.close();
+      }
+    });
+    await client.insert({doc: doc1, stream, invocationSigner, keyResolver});
+    const doc = new EdvDocument({
+      invocationSigner,
+      id: doc1.id,
+      keyAgreementKey: client.keyAgreementKey,
+      capability: {
+        id: `${client.id}`,
+        invocationTarget: `${client.id}/documents/${doc1.id}`
+      }
+    });
+    const result = await doc.read();
+    result.should.be.an('object');
+    result.content.should.deep.equal({indexedKey: 'value1'});
+    should.exist(result.stream);
+    result.stream.should.be.an('object');
+    const expectedStream = await doc.getStream({doc: result});
+    const reader = expectedStream.getReader();
+    let streamData = new Uint8Array(0);
+    let done = false;
+    while(!done) {
+      // value is either undefined or a Uint8Array
+      const {value, done: _done} = await reader.read();
+      // if there is a chunk then we need to update the streamData
+      if(value) {
+        // create a new array with the new length
+        const next = new Uint8Array(streamData.length + value.length);
+        // set the first values to the existing chunk
+        next.set(streamData);
+        // set the chunk's values to the rest of the array
+        next.set(value, streamData.length);
+        // update the streamData
+        streamData = next;
+      }
+      done = _done;
+    }
+    console.log('streamData', {streamData, data});
   });
 
 });
