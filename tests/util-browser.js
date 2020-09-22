fc21409180b070b18bf6eea8a67c5ef3152b52ff
Matthew Collier <mcollier@digitalbazaar.com>
2020-09-22 13:49:18 -0400

    Remove todo.

--- tests/util-browser.js
+++ tests/util-browser.js
@@ -1,10 +1,8 @@
-
 // eslint-disable-next-line no-undef
 let ReadableStream = window.ReadableStream;
 // eslint-disable-next-line no-undef
 let TransformStream = window.TransformStream;
 
-// TODO: ensure this polyfill is necessary
 import {ReadableStream as Readable, TransformStream as Transform}
   from 'web-streams-polyfill/ponyfill';
 if(!ReadableStream) {
