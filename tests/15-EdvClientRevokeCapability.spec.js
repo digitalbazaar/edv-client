5751693f785553e9255ec94f67a2d5a4ba8e331e
Dave Longley <dlongley@digitalbazaar.com>
2021-09-01 12:07:55 -0400

    Update `revokeCapability`.

--- tests/15-EdvClientRevokeCapability.spec.js
+++ tests/15-EdvClientRevokeCapability.spec.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright (c) 2018-2020 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2018-2021 Digital Bazaar, Inc. All rights reserved.
  */
 import {CapabilityDelegation} from '@digitalbazaar/zcapld';
 import {createRecipient, JWE_ALG} from './test-utils.js';
